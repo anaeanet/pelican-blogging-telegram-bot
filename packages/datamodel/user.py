e7c97aeacac427fe0984cbcbde59efab17581695
anaeanet <anja@anaea.net>
2017-06-03 23:36:31 +0200

    preparing data model to maintain and administer blog posts and all related data

--- packages/datamodel/user.py
+++ packages/datamodel/user.py
@@ -4,4 +4,13 @@ __author__ = 'anaeanet'
 class User:
 
     def __init__(self, user_id, state):
-        None
+        self.__user_id = user_id
+        self.__state = state
+
+    @property
+    def id(self):
+        return self.__user_id
+
+    @property
+    def state(self):
+        return self.__state
