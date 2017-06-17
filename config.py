131d8f5e0fe8d10a39f6114d0179da5d4288be6d
anaeanet <anja@anaea.net>
2017-06-17 16:10:30 +0200

    updated config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ __author__ = 'anaeanet'
 
 url = "https://api.telegram.org/bot"
 file_url = "https://api.telegram.org/file/bot"
-token = "<TOKEN>"
+token = "<BOT-TOKEN>"
 database_name = "database.sqlite"
-authorized_users = [<USER_ID1>, <USER_ID2>, ...]
+authorized_users = [{"id":<USER1_ID>, "name":"<USER1_NAME>"}, ...]
 
-markdown_target = ".."
-gallery_target = ".."
+markdown_target = "<md_target_DIR>"
+gallery_target = "<gallery_target_DIR>"
