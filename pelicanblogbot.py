e098cc228dd48b4129443d92b307678ba35d567a
anaeanet <anja@anaea.net>
2017-06-18 12:44:37 +0200

    pulled logging config out into its own logging.conf file, renamed main script

---
+++ pelicanblogbot.py
@@ -0,0 +1,47 @@
+from packages.persistence.dbwrapper import DBWrapper
+from packages.bot.pelicanmarkdownbot import PelicanMarkdownBot
+from packages.bot.parsemode import ParseMode
+
+import logging
+import logging.config
+import config
+
+__author__ = "anaeanet"
+
+
+def main():
+    logging.config.fileConfig('logging.conf')
+    logger = logging.getLogger("pelicanBlogBot")
+
+    logger.info("starting pelicanBlogBot")
+
+    try:
+        token = config.token
+        url = config.url
+        file_url = config.file_url
+        database = DBWrapper(config.database_name)
+        authorized_users = config.authorized_users
+
+        post_target_url = config.markdown_target
+        gallery_target_url = config.gallery_target
+
+        bot = PelicanMarkdownBot(token, url, file_url, database, post_target_url, gallery_target_url,
+                                 authorized_users=authorized_users)
+        # start bot
+        bot.run()
+
+    except (NameError, AttributeError) as e:
+        logger.exception("Config incomplete, PelicanMarkdownBot could not be instantiated!")
+        raise
+
+    except Exception as e:
+        logger.exception("Bot crashed!")
+
+        # inform all authorized users about crashing blogging bot
+        for user_id in authorized_users:
+            bot.send_message(user_id, "BloggingBot was terminated unexpectedly!", parse_mode=ParseMode.HTML.value)
+
+        raise
+
+if __name__ == "__main__":
+    main()
